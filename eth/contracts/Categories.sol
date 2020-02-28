c4b67a29129b0d29ba38696170e563f32e5815f9
Victor Porton <porton@narod.ru>
2020-02-29 00:13:04 +0200

    voting

--- eth/contracts/Categories.sol
+++ eth/contracts/Categories.sol
@@ -1,16 +1,25 @@
 pragma solidity ^0.5.0;
 
-contract Categories {
+import './BaseToken.sol';
 
+contract Categories is BaseToken {
+
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+    string public version = 'U1';
+
+    uint maxId = 0;
+    
     event FileAdded(uint256 indexed id, uint indexed version, string name, string description);
 
     struct Subcategory {
-        uint256 id;
+        Category category;
         int256 votes;
     }
 
-    //struct FileVersion {
-    //}
+    // voter => (issue => value)
+    mapping (address => mapping (uint256 => int256)) voters;
     
     struct File {
         uint256 id;
@@ -19,11 +28,40 @@ contract Categories {
         uint256 eventBlock;
     }
     
-    struct CategoryInfo {
-        Subcategory[] subcategories;
+    struct Category {
+        uint256 id;
+        uint256[] subcategories;
         File[] files;
     }
 
     mapping (uint256 => File) private files;
-    mapping (uint256 => CategoryInfo) private structure;
+    mapping (uint256 => Subcategory) private categories;
+
+/// ERC-20 ///
+
+    constructor() public {
+        name = "Voting";
+        decimals = 8;
+        symbol = "VOT";
+    }
+
+    function() payable external {
+        totalSupply = msg.value;
+        balances[msg.sender] += msg.value; // 1/1 exchange rate
+        emit Transfer(address(this), msg.sender, msg.value);
+    }
+
+/// Voting ///
+
+    function voteForFile(uint256 _issue, bool _yes) external {
+        int256 _value = _yes ? int256(balances[msg.sender]) : -int256(balances[msg.sender]);
+        files[_issue].votes += -voters[msg.sender][_issue] + _value; // reclaim the previous vote
+        voters[msg.sender][_issue] = _value;
+    }
+
+    function voteForCategory(uint256 _issue, bool _yes) external {
+        int256 _value = _yes ? int256(balances[msg.sender]) : -int256(balances[msg.sender]);
+        categories[_issue].votes += -voters[msg.sender][_issue] + _value; // reclaim the previous vote
+        voters[msg.sender][_issue] = _value;
+    }
 }

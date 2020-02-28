3cbe880220e7dc2238f8e57c027f176b7c3d26d4
Victor Porton <porton@narod.ru>
2020-02-28 23:37:16 +0200

    voting contract

--- eth/contracts/Voting.sol
+++ eth/contracts/Voting.sol
@@ -9,6 +9,12 @@ contract Voting is BaseToken {
     string public symbol;
     string public version = 'U1';
 
+    // issue => votes
+    mapping (uint256 => int256) votes;
+    
+    // voter => (issue => value)
+    mapping (address => mapping (uint256 => int256)) voters;
+    
     constructor() public {
         name = "Voting";
         decimals = 8;
@@ -19,5 +25,11 @@ contract Voting is BaseToken {
         totalSupply = msg.value;
         balances[msg.sender] += msg.value; // 1/1 exchange rate
         emit Transfer(address(this), msg.sender, msg.value);
+    }
+
+    function vote(uint256 _issue, bool _yes) external {
+        int256 _value = _yes ? int256(balances[msg.sender]) : -int256(balances[msg.sender]);
+        votes[_issue] += -voters[msg.sender][_issue] + _value; // reclaim the previous vote
+        voters[msg.sender][_issue] = _value;
     }
 }
